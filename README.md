241e54ce27e29f758df6ce39b1ea4edd5dad75e8
wilgai <46855991+wilgai@users.noreply.github.com>
2023-12-18 17:30:28 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ngx-intl-tel-input-demo-ng-9-nhumpb
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/wilgai/ngx-intl-tel-input-demo-ng-9-nhumpb)
---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { SearchCountryField, CountryISO, PhoneNumberFormat } from 'ngx-intl-tel-input';
+
+@Component({
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+	separateDialCode = false;
+	SearchCountryField = SearchCountryField;
+	CountryISO = CountryISO;
+  PhoneNumberFormat = PhoneNumberFormat;
+	preferredCountries: CountryISO[] = [CountryISO.UnitedStates, CountryISO.UnitedKingdom];
+	phoneForm = new FormGroup({
+		phone: new FormControl(undefined, [Validators.required])
+	});
+
+	changePreferredCountries() {
+		this.preferredCountries = [CountryISO.India, CountryISO.Canada];
+	}
+}
